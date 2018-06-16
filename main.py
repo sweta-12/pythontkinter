3b55fbb8c02db77de55a2768756e273adeb12913
Naman Nagwani <namannagwani@gmail.com>
2018-06-16 21:26:38 +0530

    UI changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Label, Tk, Button, Entry, Frame
+from tkinter import Label, Tk, Button, Entry, Frame, PhotoImage, RIGHT, LEFT
 
 from admin_login import AdminLogin
 from user_login import UserLogin
@@ -9,13 +9,23 @@ class MainWindow():
 		self.frame = Frame(self.master)
 
 		self.label = Label(self.master, text="Mind Clock")
+
+		img=PhotoImage(file="assets/admin.gif")
+		img1=PhotoImage(file="assets/user.gif")
+
+		self.label = Label(self.master, text="Welcome to the Mind Clock Game")
+		self.label.config(bg='#34af23')
 		self.label.pack()
 
-		self.admin_button = Button(self.master, text="Admin", command=self.admin_window)
-		self.admin_button.pack()
+		self.admin_button = Button(self.master, image=img, text= "admin", command=self.admin_window)
+		self.admin_button.image=img
+		self.admin_button.config(bg='#34af23')
+		self.admin_button.pack(side=LEFT)
 
-		self.user_button = Button(self.master, text="User", command=self.user_window)
-		self.user_button.pack()
+		self.user_button = Button(self.master, image=img1, text= "user", command=self.user_window)
+		self.user_button.image=img1
+		self.user_button.config(bg='#34af23')
+		self.user_button.pack(side=RIGHT)
 
 		self.quit = Button(self.master, text="Exit", command=master.quit)
 		self.quit.pack()
@@ -32,5 +42,8 @@ class MainWindow():
 		print("user window")
 
 root = Tk()
+root.geometry('500x400')
+root.config(bg='#34af23')
+root.title("Mindclock")
 main_window = MainWindow(root)
 root.mainloop()
