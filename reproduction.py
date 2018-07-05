ccede038b9c54a3c95af1358d477199271b3d808
noirheeta <noirheeta8@hotmail.com>
2018-07-06 01:26:16 +0530

    initial stage of reproduction.py

--- reproduction.py
+++ reproduction.py
@@ -2,16 +2,29 @@ from tkinter import *
 
 class Reproduction(Toplevel):
 	def __init__(self,master=None):
+		self.master = master
 		self.testcase_master = Toplevel(master)
-	
+		self.testcase_master.bind("<space>", self.show_ball)
+
 
 		
 		self.canvas = Canvas(self.testcase_master, width = 350, height = 300)
 		self.canvas.pack()
 
-		img = PhotoImage(file="assets/clock.gif")
-		self.canvas.create_image(60,40, anchor=NW, image=img)
+		#img = PhotoImage(file="assets/clock.gif")
+		#self.canvas.create_image(60,40, anchor=NW, image=img)
+	
 
-		mainloop()
+	def show_ball(self,event=None):
+		
+		self.canvas.create_oval(200,200,70,70,fill='#6666ff')
+		self.canvas.create_text(134,134,text="R",fill="white",font=("tahoma",30))
+		self.canvas.pack()
+		self.testcase_master.bind("<KeyRelease>", self.hide_ball)
+
+	def hide_ball(self,event=None):
+		self.canvas.destroy()
+
+
 
 
